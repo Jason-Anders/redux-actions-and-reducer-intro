a8e9bb00b7701c654d1b51584167f492aa7597e4
Jason Anders <159067242+Jason-Anders@users.noreply.github.com>
2025-09-05 15:06:51 -0400

    Initial commit

---
+++ src/store/articleReducer.js
@@ -0,0 +1,30 @@
+import articles from "../data/data.json";
+
+// console.log(articles);
+// define variable
+export const LOAD_ARTICLES = "article/loadArticles";
+
+// action creator function
+export const loadArticles = () => {
+  return {
+    type: LOAD_ARTICLES,
+    articles,
+  };
+};
+
+const initialState = {
+  entries: [],
+  isLoading: true,
+};
+
+// create article reducer function
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_ARTICLES:
+      return { ...state, entries: [...action.articles] };
+    default:
+      return state;
+  }
+};
+
+export default articleReducer;
